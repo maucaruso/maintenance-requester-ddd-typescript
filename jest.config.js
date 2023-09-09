bc72b9463836d6ceb5162f67f2cc137cda6893b7
maucaruso <carusojr@outlook.com>
2023-09-09 19:18:07 -0300

    chore: :package: update: jest and ts settings

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/__tests__'],
+  moduleDirectories: ['node_modules', 'src'],
+  testMatch: ['**/*.test.ts'],
+};
