99ef2cfae684d6cf12a6afa94af030862400cfa0
maucaruso <carusojr@outlook.com>
2023-09-20 08:17:27 -0300

    chore: :wrench: update: adjusting jest and ts aliases

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.json");
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/src", "<rootDir>/__tests__"],
   moduleDirectories: ["node_modules", "src"],
   testMatch: ["**/*.test.ts"],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
+    "^@infra/(.*)$": "<rootDir>/src/infra/$1",
+    "^@domain/(.*)$": "<rootDir>/src/domain/$1",
+    "^@app/(.*)$": "<rootDir>/src/app/$1",
+    "^@presentation/(.*)$": "<rootDir>/src/presentation/$1",
+  },
 };
